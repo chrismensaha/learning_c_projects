394a9201ba57c11c4fa686cf735cb190854f8cd3
chrismensaha <chrisanin76563@gmail.com>
2026-02-03 16:25:50 -0500

    Add scanf buffer

--- food_log/food-log.c
+++ food_log/food-log.c
@@ -1,15 +1,4 @@
 #include <stdio.h>
-/*A New Struct: Create a struct Expense that holds:
-
-A char array for the item name (e.g., "Coffee").
-
-A double for the price.
-
-The Logic Loop: Ask the user how many items they want to log. 
-Use a loop to fprintf each item into a file named expenses.txt.
-
-The Calculation Phase: Open that file back up, read it line-by-line
-using fscanf or fgets, and keep a running total of the costs.*/
 
 #define STARTING_SIZE 100
 
@@ -20,18 +9,25 @@ typedef struct expense{
 
 int main(){
     Expense* expense=malloc(sizeof(Expense));
+    char* item_name=malloc(STARTING_SIZE*sizeof(char));
     char* file_name=malloc(STARTING_SIZE*sizeof(char));
+    printf("File Name: ");
+    fgets(file_name,STARTING_SIZE,stdin);
+    file_name[strcspn(file_name, "\n")]=0; 
+
     char* file_line=malloc(STARTING_SIZE*sizeof(char));
-    FILE* file=fopen("expenses.txt","w");
+    FILE* file=fopen(file_name,"w");
 
     if (file==NULL){
         free(expense);
         free(file_line);
+        free(file_name);
     }
 
     int item_number=0;
     printf("# of Items to Store: ");
     scanf("%d",&item_number);
+    while (getchar() != '\n');
 
     for (int i=0;i<=item_number;i++){
         printf("Enter Item Name: ");
@@ -40,28 +36,37 @@ int main(){
 
         printf("Enter Item Price: ");
         scanf("%.2f",&expense->price);
+        while (getchar() != '\n');
 
         fprintf(file,"%s: ",expense->item_name);
         fprintf(file,"$%.2f\n",expense->price);
 
         }
     fclose(file);
-    FILE* file=fopen("expenses.txt","r");
-    if (file==NULL){
+    FILE* file_ptr=fopen("expenses.txt","r");
+    if (file_ptr==NULL){
+        fclose(file);
         free(expense);
         free(file_line);
+        free(file_name);
     }
-    
-    while(fgets(file_line,STARTING_SIZE,file)!=NULL){
-        file_line[strcspn(file_line, "\n")]=0;
-        fprintf(file,"%s\n",file_line);
-    }
-
    
+    double total_cost=0.0;
+    int i=1;
+    while(fgets(file_line,STARTING_SIZE,file)!=NULL){
+        file_line[strcspn(file_line, "\n")]=0;       
+        total_cost+=expense->price;
+
+        printf("%s: $%.2f\n",expense->item_name,expense->price);       
+        i++;
+    }
+    printf("$%.2f\n",total_cost);
+
     fclose(file);
+    fclose(file_ptr);
     free(expense);
     free(file_line);
-
+    free(file_name);
     return 0;
 
 }
