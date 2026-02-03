da7bc80e905264cb9d1ca57bd9594520a02ab428
chrismensaha <chrisanin76563@gmail.com>
2026-02-03 14:35:04 -0500

    fix read/write logic

--- file_password_checker/file_reader.c
+++ file_password_checker/file_reader.c
@@ -5,33 +5,50 @@
 
 
 int main(){
-    Password* password=malloc(STARTING_SIZE*sizeof(password));
-    char* str=malloc(STARTING_SIZE*sizeof(char));
-    FILE *file = fopen("initial.txt","r");
-    
+    char* file_name=malloc(STARTING_SIZE*sizeof(char));
+    printf("Enter file name here: ");
+    fgets(file_name,STARTING_SIZE,stdin);
+    file_name[strcspn(file_name, "\n")] = 0;
 
-    
+    Password* password=malloc(sizeof(Password));
+    char* str=malloc(STARTING_SIZE*sizeof(char));
+    FILE *file = fopen(file_name,"r");   
+    FILE *file_ptr = fopen("filtered_file","w");
+
     if (file==NULL){
-        printf("Error Opening File!");
+        printf("Error Opening File!\n");
+        free(str);
+        free(password);
+        free(file_name);
         return 1;
     }
 
-   
-    fgets(str,STARTING_SIZE,file);
-    str[strcspn(str, "\n")] = 0;
-    password_checker(str,password);
-
-    FILE *file_ptr = fopen("filtered.txt","w");
-    for (int i=0;file_ptr!=NULL;i++){
-        fprintf(file_ptr,"%s\n",str);
-    }
-    
-    
-    char ch;
-    while (( ch = fgetc(file)) != EOF){
-    putchar(ch);
+    if (file_ptr==NULL){
+        fclose(file);
+        printf("Error Opening File!\n");
+        free(str);
+        free(password);
+        free(file_name);
+        return 1;
     }
 
+    while (fgets(str, STARTING_SIZE, file) != NULL){       
+        str[strcspn(str, "\n")] = 0;
+        password_checker(str,password);
+        if (password->has_num && password->has_upper && password->is_secure_length){
+            fprintf(file_ptr,"%s\n",str);
+        }
+    }
+
+    rewind(file);
+    int ch;
+    while (( ch = fgetc(file)) != EOF){putchar(ch);}
+
+    fclose(file_ptr);
     fclose(file);
+    free(str);
+    free(password);
+    free(file_name);
     return 0;
+
 }
