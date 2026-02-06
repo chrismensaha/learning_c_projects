8a2fea64b00910c4533d993201aa6cce17cbb6c7
chrismensaha <chrisanin76563@gmail.com>
2026-02-06 16:31:15 -0500

    Change Password struct call logic

--- file_password_checker/password-checker.c
+++ file_password_checker/password-checker.c
@@ -5,19 +5,19 @@
 
 
 
-void password_checker(char* input, Password* result){
-    result->is_secure_length=0;
-    result->has_num=0;
-    result->has_upper=0;
+void password_checker(char* input, Password result){
+    result.is_secure_length=0;
+    result.has_num=0;
+    result.has_upper=0;
     if (strlen(input)>=8){
-        result->is_secure_length=1;
+        result.is_secure_length=1;
     }
     for (int i=0;input[i]!='\0';i++){
         if (input[i]>='0' && input[i]<='9'){ 
-            result->has_num=1;
+            result.has_num=1;
         }
         if (input[i]>='A' && input[i]<='Z'){ 
-            result->has_upper=1;
+            result.has_upper=1;
         }
     }
     }
@@ -27,19 +27,22 @@ void check_password(){
     size_t size_buffer = STARTING_SIZE;
     char input[size_buffer];
     char resume='Y';
-    int change=0;
+    
     int is_running=1;
-    Password* password=malloc(sizeof(Password));
+    Password password;
     enum state {A,B};   
-
+    enum state change=A;
     while (is_running){
         switch (change){
         case A:
+            printf("Enter password to check: "); 
+            fgets(input, sizeof(input), stdin); 
+            input[strcspn(input, "\n")] = 0;           
             password_checker(input,password);
-            if (password->is_secure_length==0){printf("Please use a longer password!\n");}
-            if (password->has_num==0){printf("Please Include a Number!\n");}
-            if (password->has_upper==0){printf("Please Include a Capital Letter!\n");}
-            if (password->is_secure_length && password->has_num && password->has_upper){
+            if (password.is_secure_length==0){printf("Please use a longer password!\n");}
+            if (password.has_num==0){printf("Please Include a Number!\n");}
+            if (password.has_upper==0){printf("Please Include a Capital Letter!\n");}
+            if (password.is_secure_length && password.has_num && password.has_upper){
                 printf("That is a strong password!\n"); 
             }
             printf("Press a key to continue | Press N or n to end: ");
@@ -57,5 +60,5 @@ void check_password(){
             break;
         }
     }
-    free(password); 
+ 
 }
